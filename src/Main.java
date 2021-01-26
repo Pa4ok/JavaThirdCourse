31bf3d44317ee6ed8f6688d3f6df22b5de4e4cf6
Pa4ok <n_leontiev@mail.ru>
2021-01-26 19:59:17 +0300

    26.01.21

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Main
+{
+    /*
+    ALT-ENTER - автоматический импорт либо вывод контекстного меню устранения ошибки
+     */
+
+    public static void main(String[] args)
+    {
+        /*System.out.println("Hello FSPO!");
+
+        int i = 0; //4 байта
+        float f = 0.33243F; //32 бита после запятой
+        double d = 0.554545D; //64 бита после запятой
+        char ch = 'a';
+        boolean b = true; //false
+        long l = 33224838543434324L; //8 байтов
+
+        Integer integer; //null
+        Float float_value; //null
+        Double double_value; //null
+        Boolean boolean_value; //null
+        Character character; //null
+        Long long_value; //null
+
+        Integer integer1 = 343;
+
+        //int[] arr1; //null
+        int[] arr1 = new int[10];
+        //System.out.println(arr1[0]);
+        System.out.println(Arrays.toString(arr1));
+
+        Integer[] arr2 = new Integer[10];
+        System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = { 4, 5, 6, 7 };
+        Integer[] arr4 = { 4, 5, 6, 7 };
+        System.out.println(Arrays.toString(arr3));
+        System.out.println(Arrays.toString(arr4));*/
+
+        /*while (true) {
+        }*/
+
+        /*for(int j=0; j< arr1.length; j++) {
+            arr1[j] = j+5;
+        }
+        System.out.println(Arrays.toString(arr1));
+
+        for(int j : arr1)
+        {
+            System.out.print(j + " | ");
+        }*/
+
+        //String s1; //null
+        String s2 = "3f4i543g453g435u3f4";
+        System.out.println(s2);
+        System.out.println(s2.toUpperCase());
+        System.out.println(s2.substring(4, 7));
+    }
+}
