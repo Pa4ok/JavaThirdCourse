3e4a9fae31adb122a8b84848d757d8ee122a0b6e
Pa4ok <n_leontiev@mail.ru>
2021-01-27 19:10:38 +0300

    27.01.21

--- src/ru/pa4ok/app/Main.java
+++ src/ru/pa4ok/app/Main.java
@@ -1,8 +1,11 @@
 package ru.pa4ok.app;
 
-import ru.pa4ok.test.Student;
-
-import java.util.Arrays;
+    /*
+    ALT-INSERT - вызов меню генерации (можно через ПКМ)
+    ALT-ENTER - вывов меню исправления ошибки (нужно перевести курсор на нее)
+    CTRL-SPACE - вызов контекстного меню автопродолжения кода
+    CTRL-ALT-L - форматирование кода (стиль)
+     */
 
 public class Main
 {
@@ -28,93 +31,25 @@ public class Main
     создать объект класса и вывести в консоль
      */
 
-    /*
-    ALT-INSERT - вызов меню генерации (можно через ПКМ)
-    ALT-ENTER - вывов меню исправления ошибки (нужно перевести курсор на нее)
-    CTRL-SPACE - вызов контекстного меню автопродолжения кода
-    CTRL-ALT-L - форматирование кода (стиль)
-     */
-
     public static void main(String[] args)
     {
-        /*String s1 = "13221223232";
-        String s2 = "13221223232";
+        Teacher teacher = new Teacher(
+                "Vasiliy Petrovi4",
+                40,
+                "math",
+                20,
+                new String[] {"223", "224", null, null, "448"}
+        );
 
-        if(s1.equals(s2)) { //если s1 будет null - вы словите NullPointerException
-
-        }*/
-
-        /*int i = 12412432;
-        boolean b = true;
-        float f = 444.44545F;
-        double d = 443343.455453D;
-        char c = 'd';
-
-        Integer integer_value = null;
-        Boolean boolean_value;
-        Float float_value;
-        Double double_value;
-        Character character_value;
-
-        int i1 = Integer.parseInt("333333");
-
-        String s; //null
-        s = "jefoeiwewfw3434";
-
-        char[] charArr = s.toCharArray();
-
-        if (1 == i1) {
-            //...
-        }
-
-        String s1 = "2323223";
-        String s2 = "2323223";
-
-        //сложные объекты нельзя сравнивать через ==
-        if(s1 == s2) {
-            //....
-        }
-
-        if(s1.equals(s2)) {
-            //...
-        }*/
-
-        /*int[] arr = new int[10];
-        System.out.println(Arrays.toString(arr));
-
-        Integer[] arr1 = new Integer[10];
-        System.out.println(Arrays.toString(arr1));*/
-
-        /*for(int i=0; i<arr.length; i++)
-        {
-            arr[i] = i;
-        }
-
-        for(int i : arr)
-        {
-            System.out.printf(i + " | ");
-        }
-        System.out.println();*/
-
-        //Integer integer = 4;
-        //String s11 = String.valueOf(4);
-
-        /*arr = new int[30];
-        System.out.println(Arrays.toString(arr));*/
-
-        /*Book book = new Book("math");
-        System.out.println(book);*/
-
-        Student student = new Student("Vasya", 15, 5);
-        System.out.println(student);
-
-        /*student.setMark(0, 4);
-        student.setMark(5, 5);
-        System.out.println(student);
-        */
-
-        student.setMark(0, 4);
-        System.out.println(student.getMark(0));
-        System.out.println(student.getMark(-1));
+        System.out.println(teacher);
+        System.out.println(teacher.hasGroup("224"));
+        System.out.println(teacher.hasGroup("22fewf4"));
+        teacher.addGroup("445");
+        teacher.addGroup("55555");
+        teacher.addGroup("vrgfgre");
+        System.out.println(teacher);
+        teacher.removeGroup("fff");
+        teacher.removeGroup("224");
+        System.out.println(teacher);
     }
 }
---
+++ src/ru/pa4ok/app/Teacher.java
@@ -0,0 +1,112 @@
+package ru.pa4ok.app;
+
+import java.util.Arrays;
+
+public class Teacher
+{
+    private String name;
+    private int age;
+    private String subject;
+    private int exp;
+    private String[] groups;
+
+    public Teacher(String name, int age, String subject, int exp, String[] groups)
+    {
+        this.name = name;
+        this.age = age;
+        this.subject = subject;
+        this.exp = exp;
+        this.groups = groups;
+    }
+
+    public Teacher(String name, int age, String subject, int exp, int groupCount)
+    {
+        this(name, age, subject, exp, new String[groupCount]);
+    }
+
+    public boolean hasGroup(String group)
+    {
+        for(String s : groups)
+        {
+            if(group.equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addGroup(String group)
+    {
+        for(int i=0; i<groups.length; i++)
+        {
+            if(groups[i] == null) {
+                groups[i] = group;
+                return;
+            }
+        }
+        System.out.println("Для группы нет места: " + group);
+    }
+
+    public void removeGroup(String group)
+    {
+        for(int i=0; i<groups.length; i++)
+        {
+            if(group.equals(groups[i])) {
+                groups[i] = null;
+                return;
+            }
+        }
+        System.out.println("Такой группы нет: " + group);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", subject='" + subject + '\'' +
+                ", exp=" + exp +
+                ", groups=" + Arrays.toString(groups) +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public String[] getGroups() {
+        return groups;
+    }
+
+    public void setGroups(String[] groups) {
+        this.groups = groups;
+    }
+}
