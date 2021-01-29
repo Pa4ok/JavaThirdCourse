8c05f205563f545b008bc4c0022415d58b903441
Pa4ok <n_leontiev@mail.ru>
2021-01-29 19:27:42 +0300

    29.01.21-3

---
+++ src/ru/pa4ok/app/Main.java
@@ -0,0 +1,47 @@
+package ru.pa4ok.app;
+
+import ru.pa4ok.test.TestClass;
+
+public class Main
+{
+    /*
+    ALT-ENTER - автоматический импорт либо вывод контекстного меню устранения ошибки
+     */
+
+    /*
+    все поля приватные + геттеры и сеттеры
+    +кастомный метод toString
+
+    Animal
+    - String title //название конкртеного вида
+    - String type  //вид, аля млекопитающее и тд
+    - int legCount
+    - int age
+    - boolean hunter
+
+    2 конструктора
+        1 - со всеми 5 полями
+
+        2 - принимает только title и type
+            вызывает первый конструктор
+            передает принятые title и type
+            а также legCount = 4, age = 1, hunter = false
+
+    создать несколько объектов класса через разные конструкторы
+    и вывести в консоль
+
+     */
+
+
+
+    public static void main(String[] args)
+    {
+        /*Book book = new Book("weghboewg", "puskin", 1725);
+        Book book1 = new Book();
+
+        System.out.println(book);
+        System.out.println(book1);*/
+
+
+    }
+}
