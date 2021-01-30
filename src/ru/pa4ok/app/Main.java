5fed8531b26662e9a8d04a9766b942b4951e6127
Pa4ok <n_leontiev@mail.ru>
2021-01-30 12:57:09 +0300

    30.01.21-3

---
+++ src/ru/pa4ok/app/Main.java
@@ -0,0 +1,44 @@
+package ru.pa4ok.app;
+
+import ru.pa4ok.test.TestClass;
+
+public class Main
+{
+    /*
+    SHIFT + F10 - быстрый запуск выбранной конфигурации (справа сверху)
+    ALT + ENTER - контектное меню исправления ошибки
+    ALT + INSERT - вызов меню генерации
+     */
+
+    /*
+    все поля приватные + геттеры и сеттеры
+    переопределенный метод toString
+
+    Car
+    - String manufacturer
+    - String mark
+    - int weight
+    - String color
+    - String number
+
+    1 конструктор
+        включает все поля
+
+    2 конструктор
+        запрашивает только manufacturer, mark, weight
+        вызывает 1 конструктор
+        педает в него все свои аргументы
+        и дополняет null номером и черным цветом
+     */
+
+    public static void main(String[] args)
+    {
+        /*Book book1 = new Book("test title", "test author", 1800);
+        Book book2 = new Book();
+
+        System.out.println(book1);
+        System.out.println(book2);*/
+
+        TestClass testClass = new TestClass();
+    }
+}
---
+++ src/ru/pa4ok/test/TestClass.java
@@ -0,0 +1,29 @@
+package ru.pa4ok.test;
+
+public class TestClass
+{
+    String test1; //в классах из 1 пакета
+    private String test2; //только в этом классее
+    public String test3; //везде
+    protected String test4; //в классах из 1 пакета + из всех классов наследников
+
+    void test1()
+    {
+
+    }
+
+    private void test2()
+    {
+
+    }
+
+    public void test3()
+    {
+
+    }
+
+    protected void setTest4()
+    {
+
+    }
+}
