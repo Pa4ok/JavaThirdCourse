679510ffb24fb4d9f770261d3fbb04f86cf9074b
Pa4ok <n_leontiev@mail.ru>
2021-02-02 18:58:14 +0300

    02.02.21

---
+++ src/ru/pa4ok/pc/NewMain.java
@@ -0,0 +1,35 @@
+package ru.pa4ok.pc;
+
+public class NewMain
+{
+    /*
+    все поля приватные + геттеры и сеттеры + toString()
+
+    Computer
+    - CPU cpu
+    - GPU gpu
+    - Memory[] memoriesBlocks
+    - 2 конструктора
+         1 - запрашивает все 3 поля
+         2 - запрашивает cpu, gpu и количество объектов оперативной пати
+    public double getRating()
+        должен расчитать рейтинг производительноти
+        по формуле cpu.maxFrequency * 0.95 * cpu.cores + gpu.memory * 0.2 + 1 (если есть rtx)
+
+    CPU //процессор
+    - String title
+    - int cores
+    - double minFrequency
+    - double maxFrequency
+
+    GPU //видеокарта
+    - String title
+    - double memory
+    - boolean rtx
+
+    Memory
+    - double memory
+    - double frequency
+
+     */
+}
