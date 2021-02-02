f5aaab5d94398228fb600634aec7fdff78ae034b
Pa4ok <n_leontiev@mail.ru>
2021-02-02 13:45:12 +0300

    02.02.21

--- src/org/edu/school/Main.java
+++ src/org/edu/school/Main.java
@@ -4,106 +4,37 @@ package org.edu.school;
 public class Main
 {
     /*
-    сделать класс учителя с пакетом org.edu.school
     все поля приватные + геттеры и сеттеры
     переопределить вывод
 
-    HumanInfo
-    - String firstname
-    - String surname
-    - String patronymic
-    - boolean isWoman
-    - int age
-    - public String getFullNames()
-        выводит фио целиком
-    - public String getShortNames()
-        выводит фио сокращенно
-    - public void nextYear()
-        метод который увеличивает год на 1
-
-
-    Teacher
-    - HumanInfo humanInfo
-    - String subject
-    - int exp (стаж)
-    - public String getTeacherInfo()
-        метод должен вернуть фио преподавателя и названиие предмета который он ведет
-    - public void nextYear()
-        вызывать humanInfo.nextYear()
-        увеличивать на 1 стаж exp++
-
-    Student
-    - HumanInfo humanInfo
-    - int level
-    - public void nextYear()
-        вызывать humanInfo.nextYear()
-        увеличивать на 1 курс level++
-
-    School
-    - int index
+    Book
     - String title
-    - Teacher[] teachers //3
-    - Student[] students //3
-    - public void nextYearAll()
-        перебирать всех студентов и преподавателей
-        и вызывать у них метод nextYear()
+    - String author
+    - int pages
+
+    Library
+    - String title
+    - Book[] books
+
+    2 конструтора
+        1 - принимат и title, и books
+        2 - принимает title и размер для массива books
+
+    public int getAllPages()
+        метод вернет сумму всех страниц всех кних
+
+    public boolean hasBook(String title)
+        проверит есть ли в массиве books книга c названием title
+
+    public Book[] getAuthorBooks(String author)
+        метод вернет новый массив книг
+        которые написаны автором author
 
     создать объект класса и протестировать функции
      */
 
     public static void main(String[] args)
     {
-        Teacher t1 = new Teacher(
-                new HumanInfo(
-                        "Vasiliy",
-                        "Grigorevich",
-                        false,
-                        50
-                ),
-                "pp",
-                30
-        );
-
-        Teacher t2 = new Teacher(
-                new HumanInfo(
-                        "Vyacheslav",
-                        "Astapovi4",
-                        false,
-                        30
-                ),
-                "math",
-                9
-        );
-
-        Student s1 = new Student(
-                new HumanInfo(
-                        "sergey",
-                        "kovalenko",
-                        false,
-                        20
-                ),
-                7
-        );
-
-        Student s2 = new Student(
-                new HumanInfo(
-                        "nikita",
-                        "efkepfewfefef",
-                        false,
-                        23
-                ),
-                5
-        );
-
-        School school = new School(23, "ГБУ им.Путина");
-        System.out.println(school);
-        school.getTeachers()[0] = t1;
-        school.getTeachers()[1] = t2;
-        school.getStudents()[0] = s1;
-        school.getStudents()[1] = s2;
-        System.out.println(school + "\n");
-
-        school.nextYearAll();
-        System.out.println(school);
+        
     }
 }
