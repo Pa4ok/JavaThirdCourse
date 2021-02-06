46607c54bbbf6c308ba3a2f7a76d64f65cd37b35
Pa4ok <n_leontiev@mail.ru>
2021-02-06 12:23:14 +0300

    06.02.21-4

---
+++ src/ru/pa4ok/SingleObjectClass.java
@@ -0,0 +1,31 @@
+package ru.pa4ok;
+
+/*
+суть патерна синглтон в том, что у вас на всю программу только 1 обьект класса
+ */
+public class SingleObjectClass
+{
+    //статичное поле в которое мы сохраняем единственный экземпляр класса
+    private static SingleObjectClass instance;
+
+    //приватный конструтор, чтобы нельзя было создать новые объекты
+    private SingleObjectClass()
+    {
+
+    }
+
+    public void test()
+    {
+        System.out.println("test");
+    }
+
+    /*
+    статичная фунция получения объекта сохраненного в статичном поле
+     */
+    public static SingleObjectClass getInstance() {
+        if(instance == null) {
+            instance = new SingleObjectClass();
+        }
+        return instance;
+    }
+}
