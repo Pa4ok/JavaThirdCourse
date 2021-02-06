43252d3e2a2248b9bbd91dfdee244461e3f01698
Pa4ok <n_leontiev@mail.ru>
2021-02-06 13:52:40 +0300

    06.02.21-7

---
+++ src/ru/pa4ok/Task.java
@@ -0,0 +1,40 @@
+package ru.pa4ok;
+
+public class Task
+{
+    /*
+    все поля приватные + геттеры сеттеры + toString()
+
+    к enum'ам добавить всложенные переменные с переводом на русский (как в примере)
+    и переопредить у них toString() выводя туда русский перевод
+
+
+    City
+    - String title
+    - CityTypeEnum type
+    - int population
+    - Building[] buildings
+
+    enum CityTypeEnum
+    - VILLAGE
+    - URBAN_VILLAGE
+    - TOWN
+    - METROPOLIS
+
+    Building
+    - Address address
+    - BuildingTypeEnum type
+    - int floorCount
+
+    enum BuildingTypeEnum
+    - SOCIAL
+    - LIVING
+    - INDUSTRIAL
+
+    Address
+    - String street
+    - int index
+    - int subIndex
+
+     */
+}
