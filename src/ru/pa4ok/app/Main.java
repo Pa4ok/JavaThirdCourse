d2e406fe3b686c0e910ccb70a2c951849482c683
Pa4ok <n_leontiev@mail.ru>
2021-02-26 10:34:32 +0300

    26.02.21

--- src/ru/pa4ok/app/Main.java
+++ src/ru/pa4ok/app/Main.java
@@ -2,6 +2,42 @@ package ru.pa4ok.app;
 
 public class Main
 {
+    /*
+    если класс наследуется кем либо - все поля protected
+    если класс НЕ наследуется - все поля private
+    ко всем классам геттеры и сеттеры + toString()
+
+    Human
+    - String firstname;
+    - String surname;
+    - String patronymic;
+    - int age;
+
+    Teacher extends Human
+    - String subject
+    - int exp
+
+    Student extends Human
+    - int level
+
+    Building
+    - String address
+    - int floorCount
+
+    School extends Building
+    - int index
+    - String title
+    - Teacher[] teachers
+    - Student[] students
+    - public boolean addEntity(Human human)
+            если класс объекта human принадлежит классу Teacher
+            то нужно добавить его на пустое месте в массиве teachers и вернуть true
+            если класс объекта human принадлежит классу Student
+            то нужно добавить его на пустое месте в массиве students и вернуть true
+            если и не то и не то - вывести в консоль аля not a school entity + human
+            если пустого места нет или у нас "not a school entity" то вернуть false
+     */
+
     public static void main(String[] args)
     {
         /*Human human = new Human("g", "g", "g", 64);
