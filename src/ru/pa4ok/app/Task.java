6cf3fcce34e6562c3497565b426ce9d45c9c5ef6
Pa4ok <n_leontiev@mail.ru>
2021-03-05 15:52:37 +0300

    05.03.21-3

---
+++ src/ru/pa4ok/app/Task.java
@@ -0,0 +1,35 @@
+package ru.pa4ok.app;
+
+public class Task
+{
+    /*
+    class LinkedEntity
+    - private static int idCounter = 0;
+    - final int id = ++idCounter;
+    - LinkedEntity child
+
+    - public void add(LinkedEntity entity)
+            дойти до такого child элемента
+            у которого его child будет null
+            и поставить entity на его место
+
+    - public boolean delete(int id)
+            дойти до такого child элемента
+            у которого будет нужный id
+            и удалить его, но при этом поставить на его место
+            его child объет
+
+    - public boolean contains(int id)
+            проверка на то есть ли такой объект в списке
+
+    - public int size()
+            итоговое количество объетов
+
+    задача реализовать свой связанный список
+     */
+
+    public static void main(String[] args)
+    {
+
+    }
+}
--- src/ru/pa4ok/app/zoo/IPredator.java
+++ src/ru/pa4ok/app/zoo/IPredator.java
@@ -1,4 +1,4 @@
-package ru.pa4ok.app;
+package ru.pa4ok.app.zoo;
 
 public interface IPredator
 {
