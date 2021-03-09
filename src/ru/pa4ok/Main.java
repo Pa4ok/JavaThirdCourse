891c0ab4508e999df30f3583bd278530a989108a
Pa4ok <n_leontiev@mail.ru>
2021-03-09 14:03:29 +0300

    09.03.21

--- src/ru/pa4ok/Main.java
+++ src/ru/pa4ok/Main.java
@@ -1,35 +1,135 @@
 package ru.pa4ok;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main
 {
+    /*
+    если класс наследуется кем либо - все поля protected
+    если класс НЕ наследуется - все поля private
+    ко всем классам геттеры и сеттеры + toString()
+
+    enum GenderEnum
+    - MALE
+    - FEMALE
+
+    Human
+    - String firstname
+    - String surname
+    - String patronymic
+    - GenderEnum gender
+    - int age
+    - public void nexYear() //age++
+
+    Teacher extends Human
+    - String subject
+    - int exp
+    - public void nexYear() //сначала вызывать родительскую реализацию, потом exp++
+
+    Student extends Human
+    - int level
+    - public void nexYear() //сначала вызывать родительскую реализацию, потом level++
+
+    Building
+    - String address
+    - int floorCount
+
+    School extends Building
+    - int index
+    - String title
+    - List<Teacher> teachers
+    - List<Student> students
+    - public boolean addEntity(Human human)
+            если класс объекта human принадлежит классу Teacher
+            то нужно добавить его в teachers и вернуть true
+            если класс объекта human принадлежит классу Student
+            то нужно добавить его в students и вернуть true
+            если и не то и не то - вывести в консоль аля not a school entity + human
+            и вернуть false
+    - public int nextYearAll()
+        перебирает всех студентов и преподавателей и вызывает у них nextYear()
+        если после вызова метода у стундента курс > 5, то его нужно удалить из списка
+        и вывести информацию о том, что он закончил вуз
+        если после вызова метода у преподавателя возраст > 65, то его нужно удалить из списка
+        и вывести информацию о том, что он вышел на пенсию
+        возвращаемым значением функции будет количество ушедших людей из школы
+     */
+
     public static void main(String[] args)
     {
-        Note note = new Note(
-                "[roewgjperpr",
-                WeekDayEnum.SUNDAY
-        );
+        /*List<String> list = new ArrayList<>();
 
-        System.out.println(note);
-        System.out.println(note.getWeekDay() == WeekDayEnum.TUESDAY);
-        System.out.println(note.getWeekDay().getRus());
+        list.add("oirgoheorg");
+        list.add("yukyukyukyuk");
+        list.add("657567657");
+        list.add("ytjth6oirgo");
 
-        /*switch (note.getWeekDay())
-        {
-            case FRIDAY:
-                break;
-            case TUESDAY:
-                break;
-        }*/
+        //System.out.println(list.size());
+        //System.out.println(list.isEmpty());
 
-        for(WeekDayEnum w : WeekDayEnum.values()) {
-            System.out.println(w);
+        System.out.println(list.contains("oirgoheorg"));
+        list.remove(2);
+        list.remove("oirgoheorg");
+        System.out.println(list.contains("oirgoheorg"));*/
+
+        //List<String> list2 = new ArrayList<>();
+        //list2.addAll(list);
+        //
+        //List<String> list2 = new ArrayList<>(list);
+
+        //более правильный способ перебора листов
+        /*for(String s : list) {
+            //...
         }
 
-        String s = "FRIDAY";
-        WeekDayEnum fr = WeekDayEnum.valueOf(s);
-        System.out.println(fr.getRus());
-        System.out.println(fr.ordinal());
-        int i = fr.ordinal();
-        String s2 = fr.name();
+        for(int i=0; i<list.size(); i++) {
+            String s = list.get(i);
+            //...
+        }*/
+
+        //System.out.println(list);
+
+        //java 9+
+        //List<String> l = List.of("eijfe", "rgrgrg", "rgeggre");
+
+        //java 8
+        //List<String> l = Arrays.asList("eijfe", "rgrgrg", "rgeggre");
+        //строка ниже вызывает ошибку, так как в возвращаемой релизации
+        //нет метода add
+        //l.add("rthytjjyjy");
+        //вариант ниже - самый простой фикс
+        //List<String> l = new ArrayList<>(Arrays.asList("eijfe", "rgrgrg", "rgeggre"));
+
+        //в листах нельзя хранить примитивы
+        //List<int> list333 = new ArrayList<>();
+        //List<Integer> list333 = new ArrayList<>();
+    }
+}
+
+class TestClass1
+{
+    private List<String> list;
+
+    public TestClass1(List<String> list) {
+        this.list = list;
+    }
+}
+
+class TestClass2
+{
+    private List<String> list = new ArrayList<>();
+
+    public TestClass2() {
+    }
+}
+
+class TestClass3
+{
+    private List<String> list;
+
+    public TestClass3(String... strings) {
+        list = new ArrayList<>(Arrays.asList(strings));
     }
 }
